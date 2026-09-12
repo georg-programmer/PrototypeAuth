3f67e0bf2c220d10fa521523f2d16d8a39e0341a
georg-programmierer <g.fink@hak-bregenz.at>
2026-09-12 13:36:00 +0200

    Small changes

--- app/(frontend)/login/page.tsx
+++ app/(frontend)/login/page.tsx
@@ -5,7 +5,7 @@ const Login = () => {
 		<form
 			action={async (formData) => {
 				"use server";
-				await signIn("credentials", formData);
+				await signIn("credentials", { ...Object.fromEntries(formData), redirectTo: "/" });
 			}}
 		>
 			<label>
--- proxy.ts
+++ proxy.ts
@@ -1,8 +1,9 @@
 import { auth } from "@/auth";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export default auth((req) => {
-  const isLoggedIn = !!req.auth;
+export default async function proxy(req: NextRequest) {
+  const session = await auth();
+  const isLoggedIn = !!session;
   const isLoginPage = req.nextUrl.pathname === "/login";
 
   if (!isLoggedIn && !isLoginPage) {
@@ -12,7 +13,7 @@ export default auth((req) => {
   if (isLoggedIn && isLoginPage) {
     return NextResponse.redirect(new URL("/", req.url));
   }
-});
+}
 
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
