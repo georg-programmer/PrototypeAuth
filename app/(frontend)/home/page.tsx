5c24745cf66fd1a372fb27c187549a134f721d8a
Gabelfisch <gabrielsinz@gmx.at>
2026-09-12 13:21:11 +0200

    Add frontend pages with home and login

---
+++ app/(frontend)/home/page.tsx
@@ -0,0 +1,8 @@
+
+const Home = () => {
+  return (
+    <h1>asdf</h1>
+  );
+};
+
+export default Home;
--- app/login/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function LoginPage() {
-  return (
-    <main>
-      {/* Login-Formular kommt hier rein */}
-    </main>
-  );
-}
