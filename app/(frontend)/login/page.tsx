b2617f80c9c25f595d7984c06ec22c832873539d
georg-programmierer <g.fink@hak-bregenz.at>
2026-09-19 09:57:55 +0200

    Bug Fix

--- app/(frontend)/login/page.tsx
+++ app/(frontend)/login/page.tsx
@@ -5,7 +5,7 @@ const Login = () => {
 		<form
 			action={async (formData) => {
 				"use server";
-				await signIn("credentials", formData, {redirectto: "/home"});
+				await signIn("credentials", formData, { redirectTo: "/home" });
 			}}
 		>
 			<label>
